3e5485130d153c74e709e0fb27f9e47975959854
Hellionjan21 <elyonjohnb@outlook.com>
2018-07-10 15:59:03 +0800

    RNutsandBolts.R

---
+++ R Nuts and Bolt/RNutsandBolts.R
@@ -0,0 +1,38 @@
+x <- 64000
+y <- 64000
+z=x+y
+z
+c <- c(0.5,"a",TRUE,5)
+class(a)
+b <- 1:10
+as.numeric(b)
+as.numeric(c)
+class(c)
+as.complex(b)
+as.complex(c)
+as.numeric(hi)
+as.complex(hi)
+as.integer(waddup)
+as.logical(waddup)
+
+x <- matrix(nrow=5, ncol=5)
+x
+dim(x)
+attributes(x)
+x <- matrix(1:25, nrow = 5, ncol = 5)
+x
+x <- 1:25
+x
+dim(x) < c(5,5)
+x
+m <- 1:10
+dim(m) <- c(2,5)
+m
+x <- factor(c("asdfghkl","asdfghkl","asdfghkm","asdfghkl"))
+x
+table(x)
+unclass(x)
+y <- factor(c("zup","yo","zup","yo"))
+y
+table(y)
+unclass(y)
